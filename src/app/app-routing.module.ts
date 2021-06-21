1c5e9746acf5aaaf1ed9cfb9232ecaa28dedbfa1
Brigitte <u201914955@upc.edu.pe>
2021-06-21 18:04:30 -0500

    add routing in the space it should render

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListTutorshipsComponent} from "./pages/tutor/list-tutorships/list-tutorships.component";
 import {MainContentComponent} from "./pages/coordinator/main-content/main-content.component";
+import {PerformanceComponent} from "./pages/tutor/performance/performance.component";
+import {ListWorkshopsStudentComponent} from "./pages/student/list-workshops-student/list-workshops-student.component";
 
 const routes: Routes = [
   { path: 'tutorias', component: ListTutorshipsComponent },
-  { path: 'coordinator/:id/main', component: MainContentComponent },
+  { path: 'coordinators/:id/main', component: MainContentComponent },
+  {path:'tutors/:id/performance', component: PerformanceComponent},
+  {path:'students/:id/workshops/new', component: ListWorkshopsStudentComponent},
+
 ];
 
 @NgModule({
